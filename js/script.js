e56dabf57564dd18eeb3ebbd1b5b5b7435f82c69
Samanta Gonzales <samanta493@gmail.com>
2017-09-01 12:23:37 -0500

    Agregar efectos a div de planes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 function validacion_log(){
   sweetAlert("Oops...", "Usuario no existe", "error");
 }
+
+$('.card').click(function(){
+    $(this).removeClass('desactivo');
+    $(this).addClass('activo');
+    setTimeout(function(){
+        $(this).removeClass('activo');
+        $(this).addClass('desactivo');
+    }, 100);
+});
